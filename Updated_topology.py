308c960b00912d19284828721c8f490c40819a43
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-02-20 15:38:16 +0100

    Update Updated_topology.py

--- Updated_topology.py
+++ Updated_topology.py
@@ -41,9 +41,8 @@ def create_linear_topology():
             # Get the IP address of h2
             h2_ip = net.get('h2').IP()
             # Run iperf test from host 1 to host 2 using the IP address and print the results
-            # running the test for 1 second
-            result = net.get('h1').cmd(f'iperf -c {h2_ip} -u -i 0.7 -b 10m')
-            # result = net.get('h1').cmd(f'iperf -c {h2_ip} -i 0.7 -b 10m')
+            # running the test for 1 secon
+            result = net.get('h1').cmd(f'iperf -c {h2_ip} -i 0.7 -b 10m')
             
             # Writing the result to the file with a separator for readability
             results_file.write(f"Test {i+1}:\n{result}\n")

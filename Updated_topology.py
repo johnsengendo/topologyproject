8dd584b1374bf6935a0f0f497ab7c4fedfd9f371
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-02-21 09:05:57 +0100

    Update Updated_topology.py

--- Updated_topology.py
+++ Updated_topology.py
@@ -32,7 +32,7 @@ def create_linear_topology():
 
     # Running iperf multiple times between the hosts
     # Opening a file in append mode
-    with open('iperf_results.txt', 'a') as results_file:
+    with open('iperf_results_1.txt', 'a') as results_file:
         for i in range(10):  # running the test 5 times
             # Starting the iperf server on host 2
             server = net.get('h2').popen('iperf -s')
@@ -42,7 +42,7 @@ def create_linear_topology():
             h2_ip = net.get('h2').IP()
             # Run iperf test from host 1 to host 2 using the IP address and print the results
             # running the test for 1 secon
-            result = net.get('h1').cmd(f'iperf -c {h2_ip} -i 1 -b 10m')
+            result = net.get('h1').cmd(f'iperf -c {h2_ip} -i 0.5 -b 10m')
             
             # Writing the result to the file with a separator for readability
             results_file.write(f"Test {i+1}:\n{result}\n")

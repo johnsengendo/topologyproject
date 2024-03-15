65dd0ebf9c97746ca2a26f32da1a8ef75c6ffe3b
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-03-15 11:12:14 +0100

    Add files via upload

---
+++ traffic_slicing_controller.py
@@ -0,0 +1,82 @@
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+class TrafficSlicing(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(TrafficSlicing, self).__init__(*args, **kwargs)
+        # out_port = slice_to_port[dpid][in_port]
+        self.slice_to_port = {
+            # Assuming slice 1 is for HTTP traffic (hosts h1, h2), slice 2 is for Video traffic (hosts h3, h4)
+            # and slice 3 is for management traffic (host h5)
+            # Root switch s1
+            1: {1: 2, 2: 1, # Slice 1 (HTTP)
+                3: 4, 4: 3, # Slice 2 (Video)
+                5: 1},      # Slice 3 (Management)
+            # Switch s2 connected to HTTP hosts
+            2: {1: 2, 2: 1},
+            # Switch s3 with one HTTP host and parent connection to s1 and child connection to s4
+            3: {1: 2, 2: 1, 3: 4, 4: 3},
+            # Switch s4 connected to Video host h4
+            4: {1: 2, 2: 1}
+        }
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install the table-miss flow entry.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=-1):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst, buffer_id=buffer_id)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        in_port = msg.match['in_port']
+        dpid = datapath.id
+
+        if dpid in self.slice_to_port and in_port in self.slice_to_port[dpid]:
+            out_port = self.slice_to_port[dpid][in_port]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)

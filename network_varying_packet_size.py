781682ee45cf83bccd07f88636ffbf70c32b059d
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-03-20 15:47:43 +0100

    Update network_varying_packet_size.py

--- network_varying_packet_size.py
+++ network_varying_packet_size.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     net.start()
 
     # Run the parallel Iperf tests with packet size 64
-    run_parallel_iperf(net, 1024)
+    run_parallel_iperf(net, 64)
 
     CLI(net)
     net.stop()

27e051869a3c33e3dbb4ad82c8dcc5698e72ab4a
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-03-24 19:36:32 +0100

    Update network_1.py

--- network_1.py
+++ network_1.py
@@ -10,58 +10,56 @@ import time
 import re
 
 class NetworkSlicingTopo(Topo):
+    """
+    A class defining a network topology for network slicing using Mininet.
+    """
     def __init__(self):
-        # Initialize topology
+        # Initializing the topology with the Topo class from Mininet
         Topo.__init__(self)
 
-        # Create template host, switch, and link
-        host_config = dict(inNamespace=True)
-        http_link_config = dict(bw=1)
-        video_link_config = dict(bw=10)
-        host_link_config = dict()
+        # Creating template configurations for hosts, switches, and links
+        host_config = dict(inNamespace=True) # Host configuration: using network namespaces
+        link_config = dict(bw=10) # Link configuration: setting bandwidth to 10 Mbps
+        host_link_config = dict(bw=10) # Host-switch link configuration: setting bandwidth to 10 Mbps
 
-        # Create switch nodes
+        # Creating switch nodes
         for i in range(4):
             sconfig = {"dpid": "%016x" % (i + 1)}
             self.addSwitch("s%d" % (i + 1), **sconfig)
 
-        # Create host nodes
+        # Creating host nodes
         for i in range(4):
             self.addHost("h%d" % (i + 1), **host_config)
 
-        # Add switch links
-        self.addLink("s1", "s2", **video_link_config)
-        self.addLink("s2", "s4", **video_link_config)
-        self.addLink("s1", "s3", **http_link_config)
-        self.addLink("s3", "s4", **http_link_config)
+        # Adding switch links
+        self.addLink("s1", "s2", **link_config)
+        self.addLink("s2", "s4", **link_config)
+        self.addLink("s1", "s3", **link_config)
+        self.addLink("s3", "s4", **link_config)
 
-        # Add host links
+        # Adding host links
         self.addLink("h1", "s1", **host_link_config)
         self.addLink("h2", "s1", **host_link_config)
         self.addLink("h3", "s4", **host_link_config)
         self.addLink("h4", "s4", **host_link_config)
 
 def run_parallel_iperf(net):
-    # Open the file to write the iperf results
-    # Start iperf servers on h3 and h4
+    # Opening the file to write the iperf results
+    # Starting iperf servers on h3 and h4
     h3 = net.get('h3')
     h4 = net.get('h4')
     server_h3 = h3.popen('iperf -s')
     server_h4 = h4.popen('iperf -s')
 
-    # Wait for the servers to start
+    # Waiting for the servers to start
     time.sleep(1)
 
-    # Start iperf clients on h1 and h2 to connect to h3 and h4 respectively
+    # Starting iperf clients on h1 and h2 to connect to h3 and h4 respectively
     h1 = net.get('h1')
     h2 = net.get('h2')
     h3_ip = h3.IP()
     h4_ip = h4.IP()
     with open('iperf_parallel_results_1.txt', 'a') as results_file:
-
-        # Use Popen to run the clients so that we can run them simultaneously
-        #client_h1 = h1.popen(['iperf', '-c', h3_ip, '-t', '10'], stdout=PIPE)
-        #client_h2 = h2.popen(['iperf', '-c', h4_ip, '-t', '10'], stdout=PIPE)
 
         result_h1 = h1.cmd(f'iperf -c {h3_ip} -t 10 -i 1 -d')
         result_h2 = h2.cmd(f'iperf -c {h4_ip} -t 10 -i 1 -d')
@@ -69,11 +67,11 @@ def run_parallel_iperf(net):
         results_file.write(f"Client h1 to h3:\n{result_h1}\n-----\n")
         results_file.write(f"Client h2 to h4:\n{result_h2}\n-----\n")
 
-        # Terminate the server processes
+        # Terminating the server processes
         server_h3.terminate()
         server_h4.terminate()
 
-        # Wait for the servers to terminate
+        # Waiting for the servers to terminate
         server_h3.wait()
         server_h4.wait()
 
@@ -94,7 +92,7 @@ if __name__ == "__main__":
     net.build()
     net.start()
     
-    # Run the parallel Iperf tests
+    # Running the parallel Iperf tests
     run_parallel_iperf(net)
     
     CLI(net)
